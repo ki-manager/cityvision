49e9271eec0eae0418c856c82954cdc52ae0ba1e
Thorsten Höke <thorsten.hoeke@googlemail.com>
2026-05-15 20:31:19 +0200

    Update start.py

--- pages/start.py
+++ pages/start.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from utils.components import card
+from PIL import Image
 
 def show():
     st.title("Startseite")
+
+    logo = Image.open("c:/Users/User/OneDrive/Dokumente/GitHub/educx-python/cityvision/logo.png")
+    st.image(logo, width=600)
 
     card(
         "CityVision",
